ee6145ec942850bd1c77ecb126940dcb1d2a0cd2
VroomMates <vroommatesinfo@gmail.com>
2016-11-30 12:34:28 +0000

    ffa andy

--- public/setupMatchFunctions.js
+++ public/setupMatchFunctions.js
@@ -62,7 +62,11 @@ var config = {
 					if( (lngdiff<=.2) && (latdiff<=.2)){
 						latdiff = (1-latdiff)*6;
 						lngdiff = (1-lngdiff)*6;
-						alert("Match around " + Math.round((latdiff + lngdiff)*(2/3)) + "km with user: " + firebase.database().ref('/Users/').once(obj.uid);
+						var name;
+					 var reference = firebase.database().ref('Users/'+obj.uid).once('value').then(function(snapshot) {
+					name = snapshot.val().firstName;
+					 });
+					alert("Match around " + Math.round((latdiff + lngdiff)*(2/3)) + "km with user: " + name);
 						//alert("Match within 8km");
 						
 						break;
