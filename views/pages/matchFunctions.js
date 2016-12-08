213b158cad0994bab8e6e617fc2c3ea90d83c2a4
VroomMates <vroommatesinfo@gmail.com>
2016-12-08 00:20:30 +0000

    combined driver and pass match functions

--- views/pages/matchFunctions.js
+++ views/pages/matchFunctions.js
@@ -10,12 +10,14 @@ var config = {
 	  var userLat;
 	  var userLng;
 	  var userUID;
+	  var driver;
 	  
 	  
 	  firebase.auth().onAuthStateChanged(function(user) {
 			firebase.database().ref('Users/' + user.uid).once('value').then(function(snapshot) {
 					userLat = snapshot.val().location.lat;
 					userLng = snapshot.val().location.lng;
+					driver = snapshot.val().driver;
 					userUID = snapshot.key;
 					document.getElementById('match').disabled=false;
 			});
@@ -28,12 +30,33 @@ var config = {
 	  var matches = [];
 	  var matched = [];
 	  
-	  function matching(){
+	  function startMatch(){
+			if(driver=="yes"){
+				searchDriver();
+			}
+			else if(driver=="no"){
+				searchPass();
+			}
+	  
+	  }
+	  
+	  function matchingDriver(){
 			while(true){
 				obj=matches.pop()
 				if(obj==null) {break;}
 				else{
-					match(obj);
+					matchDriver(obj);
+				}
+			}
+			store();
+	  }
+	  
+	  function matchingPass(){
+			while(true){
+				obj=matches.pop()
+				if(obj==null) {break;}
+				else{
+					matchPass(obj);
 				}
 			}
 			store();
@@ -44,7 +67,7 @@ var config = {
 			firebase.database().ref('Users/'+userUID+'/match/').update(matched,function redirect_home() {window.location = "match.html"});
 	  }
 	  
-	  function match(obj){
+	  function matchDriver(obj){
 		var str = {//mullingar
 			lat: obj.lat,
 			lng: obj.lng
@@ -176,7 +199,7 @@ var config = {
 		}
 	  }
 	  
-	function search(){
+	function searchDriver(){
 		var mayLat = 53.3813;
 		var mayLng = -6.5918;
 		
@@ -212,6 +235,191 @@ var config = {
 				}
 			}
 		})
-		matching();
+		matchingDriver();
+		});
+	}
+	
+	function matchPass(obj){
+			
+	var str = {//rochfort
+		lat:obj.lat,
+		lng:obj.lng
+	};
+	var you = {//kinnegad
+		lat:userLat,
+		lng:userLng
+	};
+	var end = {//maynooth
+		lat:53.3813,
+		lng:-6.5918
+	};
+	
+	var poslat = you.lat;
+	var poslng = you.lng;
+	//if(str.lat<end.lat) //if below
+	//if(str.lat>end.lat) //if above
+	//if(str.lng>end.lng) //if left
+	//if(str.lng<end.lng) //if right
+	
+	//above + left
+	if(you.lat>=end.lat&&you.lng<=end.lng){
+		var area = .05;
+		var ratiolat = Math.abs(you.lat - end.lat)/100;
+		var ratiolng = Math.abs(you.lng - end.lng)/100;
+		var stoplat = (poslat + (ratiolat*300));
+		var stoplng = (poslng - (ratiolng*300));
+		while(poslat<=stoplat && poslng >= stoplng){
+			var lngdiff = Math.abs(str.lng - poslng);
+			var latdiff = Math.abs(str.lat - poslat);
+			//alert(lngdiff + " | " + latdiff);
+	
+			if( (lngdiff<area) && (latdiff<area)){
+				latdiff = Math.abs(str.lng - you.lng)*100;
+				lngdiff = Math.abs(str.lat - you.lat)*100;
+				//alert(latdiff + " | " + lngdiff);
+				matched.push(obj);
+				break;
+			}
+			
+			if(poslat<stoplat){
+				poslat += ratiolat;
+			}
+			
+			if(poslng>stoplng){
+				poslng -= ratiolng;
+			}
+			area += .001;
+			//alert(area);
+		}
+	}
+	//above + right
+	if(you.lat>=end.lat&&you.lng>=end.lng){
+		var area = .05;
+		var ratiolat = Math.abs(you.lat - end.lat)/100;
+		var ratiolng = Math.abs(you.lng - end.lng)/100;
+		var stoplat = (poslat + (ratiolat*300));
+		var stoplng = (poslng + (ratiolng*300));
+		while(poslat<=stoplat && poslng <= stoplng){
+			var lngdiff = Math.abs(str.lng - poslng);
+			var latdiff = Math.abs(str.lat - poslat);
+			//alert(lngdiff + " | " + latdiff);
+			if( (lngdiff<area) && (latdiff<area)){
+				latdiff = Math.abs(str.lng - you.lng)*100;
+				lngdiff = Math.abs(str.lat - you.lat)*100;
+				matched.push(obj);
+				break;
+			}
+			if(poslat<stoplat){
+				poslat += ratiolat;
+			}
+			
+			if(poslng<stoplng){
+				poslng += ratiolng;
+			}
+			area += .001;
+			//alert(area);
+		}
+	}
+
+	//below + left
+	if(you.lat<=end.lat&&you.lng<=end.lng){
+		var area = .05;
+		var ratiolat = Math.abs(you.lat - end.lat)/100;
+		var ratiolng = Math.abs(you.lng - end.lng)/100;
+		var stoplat = (poslat - (ratiolat*300));
+		var stoplng = (poslng - (ratiolng*300));
+		while(poslat<=stoplat && poslng >= stoplng){
+			var lngdiff = Math.abs(str.lng - poslng);
+			var latdiff = Math.abs(str.lat - poslat);
+			//alert(lngdiff + " | " + latdiff);
+		
+			if( (lngdiff<area) && (latdiff<area)){
+				latdiff = Math.abs(str.lng - you.lng)*100;
+				lngdiff = Math.abs(str.lat - you.lat)*100;
+				matched.push(obj);
+				break;
+			}
+			
+			if(poslat>stoplat){
+				poslat -= ratiolat;
+			}
+			
+			if(poslng>stoplng){
+				poslng -= ratiolng;
+			}
+			area += .001;
+			//alert(area);
+		}
+	}
+	
+	//below + right 
+	
+	if(you.lat<=end.lat&&you.lng>=end.lng){
+		var area = .05;
+		var ratiolat = Math.abs(you.lat - end.lat)/100;
+		var ratiolng = Math.abs(you.lng - end.lng)/100;
+		var stoplat = (poslat - (ratiolat*400));
+		var stoplng = (poslng + (ratiolng*400));
+		while(poslat<=stoplat && poslng >= stoplng){
+			var lngdiff = Math.abs(str.lng - poslng);
+			var latdiff = Math.abs(str.lat - poslat);
+			//alert(lngdiff + " | " + latdiff);
+			
+			if( (lngdiff<area) && (latdiff<area)){
+				latdiff = Math.abs(str.lng - you.lng)*100;
+				lngdiff = Math.abs(str.lat - you.lat)*100;
+				matched.push(obj);
+				break;
+			}
+			
+			if(poslat>stoplat){
+				poslat -= ratiolat;
+			}
+			
+			if(poslng<stoplng){
+				poslng += ratiolng;
+			}
+			area += .001;
+			//alert(area);
+		}
+	}
+	}
+	
+	
+	
+	function searchPass(){
+		var mayLat = 53.3813;
+		var mayLng = -6.5918;
+		
+		
+		firebase.database().ref().once("child_added", function(snapshot) {
+			snapshot.forEach(function(childSnapshot){
+			var uid = childSnapshot.key;
+			var lat = childSnapshot.val().location.lat;
+			var lng = childSnapshot.val().location.lng;
+			var email = childSnapshot.val().email;
+			var firstName = childSnapshot.val().firstName;
+			var lastName = childSnapshot.val().lastName;
+			var isDriver = childSnapshot.val().driver;
+			if(uid!=firebase.auth().currentUser.uid){
+				if((lat>=userLat&&lat<=mayLat)&&(lng<=userLng&&lng>=mayLng)){
+					
+				}
+				else if((lat>=userLat&&lat<=mayLat)&&(lng>=userLng&&lng<=mayLng)){
+					
+				}
+				else if((lat<=userLat&&lat>=mayLat)&&(lng<=userLng&&lng>=mayLng)){
+					
+				}
+				else if((lat<=userLat&&lat>=mayLat)&&(lng>=userLng&&lng<=mayLng)){
+					
+				}
+				else if(isDriver=="yes"){
+					var object = {uid: uid, lat:lat, lng:lng, email:email, firstName: firstName, lastName: lastName};
+						matches.push(object);
+				}
+			}
+		})
+		matchingPass();
 		});
 	}
