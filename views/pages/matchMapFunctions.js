6f6c180a14203bbaf72270fc2bb5333aac5fea50
VroomMates <vroommatesinfo@gmail.com>
2016-12-11 01:05:50 +0000

    added new start and end markers and compacted markers

--- views/pages/matchMapFunctions.js
+++ views/pages/matchMapFunctions.js
@@ -11,6 +11,7 @@ var config = {
 	  var userLat;
 	  var userLng;
 	  var map;
+	  var markers = [];
 	  
 	  function buildMap(){
 	  	map = new google.maps.Map(document.getElementById('map'), {
@@ -38,8 +39,63 @@ var config = {
 	  		  suppressMarkers: true // here
 			});
 			displayRoute(user, maynooth, directionsService,directionsDisplay);
+			var image = {
+			  url: "marker.png",
+			  size: new google.maps.Size(20, 20),
+			  origin: new google.maps.Point(0, 0),
+			  anchor: new google.maps.Point(10, 10),
+			  scaledSize: new google.maps.Size(20, 20)
+			};
+		var startMarker = new google.maps.Marker({
+			  position: user,
+			  map: map,
+			  title: ("You Are \n Here"),
+			  icon: image ,
+			  animation: google.maps.Animation.DROP
+			});
 			
+		var endMarker = new google.maps.Marker({
+			  position: maynooth,
+			  map: map,
+			  title: ("Maynooth"),
+			  icon: image,
+			  animation: google.maps.Animation.DROP
+			});
+			var infoWindow = new google.maps.InfoWindow();
+		google.maps.event.addListener(startMarker,'click', (function(startMarker, infoWindow){ 
+				return function() {
+				if (startMarker.getAnimation()!==null){
+				  startMarker.setAnimation(null);
+				  } else{
+					  startMarker.setAnimation(4);
+				  }
+					infoWindow.setContent(
+					  "<div>" +
+						"<h1>" + "You" + "</h1>" +
+					  "</div>"
+					);
+					infoWindow.open(map,startMarker);
+				};
+				
+			})(startMarker,infoWindow));
+			google.maps.event.addListener(endMarker,'click', (function(endMarker, infoWindow){ 
+				return function() {
+				if (endMarker.getAnimation()!==null){
+				  endMarkerr.setAnimation(null);
+				  } else{
+					 endMarker.setAnimation(4);
+				  }
+					infoWindow.setContent(
+					  "<div>" + 
+						"<h1>" + "Maynooth" + "</h1>" +
+					  "</div>"
+					);
+					infoWindow.open(map,endMarker);
+				};
+				
+			})(endMarker,infoWindow));
 	    while(true){
+			var overlapped=false;
 			var obj = matches.pop();
 			if(obj==null) break;
 			
@@ -47,30 +103,44 @@ var config = {
 			var name = obj.firstName + " " + obj.lastName;
 			var email = obj.email;
 			
-			var infoWindow = new google.maps.InfoWindow();
-			var marker = new google.maps.Marker({
-			  position: uluru,
-			  map: map,
-			  title: (name + "\n" + email),
-			  animation: google.maps.Animation.DROP
-			});
-			google.maps.event.addListener(marker,'click', (function(marker, name, email, infoWindow){ 
-				return function() {
-				if (marker.getAnimation()!==null){
-				  marker.setAnimation(null);
-				  } else{
-					  marker.setAnimation(4);
-				  }
-					infoWindow.setContent(
-					  "<div>" + 
-						"<h1>" + name + "</h1>" +
-						"<p>" + email + "</p>" +
-					  "</div>"
-					);
-					infoWindow.open(map,marker);
-				};
-				
-			})(marker,name, email,infoWindow));  
+			if(!markers.length==0){
+				for(var i=0;i<markers.length;i++){
+					if(Math.abs(markers[i].position.lat()-uluru.lat)<0.002&&Math.abs(markers[i].position.lng()-uluru.lng)<0.002){
+						markers[i].title+=("<div>" + 
+							"<h3>" + name + "</h3>" +
+							"<p>" + email + "</p>" +
+						  "</div>");
+						overlapped=true;
+					}
+				}
+			}
+			if(!overlapped){
+				var infoWindow = new google.maps.InfoWindow();
+				var marker = new google.maps.Marker({
+				  position: uluru,
+				  map: map,
+				  title: ("<div>" + 
+							"<h3>" + name + "</h3>" +
+							"<p>" + email + "</p>" +
+						  "</div>"),
+				  animation: google.maps.Animation.DROP
+				});
+				google.maps.event.addListener(marker,'click', (function(marker, infoWindow){
+					return function() {
+					if (marker.getAnimation()!==null){
+					  marker.setAnimation(null);
+					  } else{
+						  marker.setAnimation(4);
+					  }
+						infoWindow.setContent(
+							marker.title
+						);
+						infoWindow.open(map,marker);
+					};
+					
+				})(marker, infoWindow));
+				markers.push(marker);
+			}
 		}
 	  }
 	  function displayRoute(origin, destination, service, display) {
